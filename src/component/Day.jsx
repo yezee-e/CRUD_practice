9763da033ddffc081fb3bfe8cc98e482f877c3b1
yeji <yejipooh@gmail.com>
2022-12-02 00:33:37 +0900

    레파지토리변경

---
+++ src/component/Day.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import useFetch from '../hooks/useFetch';
+import Word from './Word';
+
+function Day() {
+  const { day } = useParams();
+
+  const words = useFetch(
+    `https://my-json-server.typicode.com/yezee-e/CRUD_practice/words?day=${day}`
+  );
+
+  return (
+    <>
+      <h2>Day {day}</h2>
+      <table>
+        <tbody>
+          {words.map((word) => (
+            <Word word={word} key={word.id} />
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+export default Day;
---
+++ src/component/DayList.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import useFetch from '../hooks/useFetch';
+
+function DayList() {
+  const days = useFetch(
+    'https://my-json-server.typicode.com/yezee-e/CRUD_practice/days'
+  );
+
+  if (days.length === 0) {
+    //로딩이 느릴경우(network에 slow3G로 확인해볼수있다)
+    return <span>Loading...</span>;
+  }
+
+  return (
+    <ul className='list_day'>
+      {days.map((day) => (
+        <li key={day.id}>
+          <Link to={`/day/${day.day}`}>Day {day.day}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default DayList;
